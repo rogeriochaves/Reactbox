be84ac594b3a7fecb2f946692da46ea7417b2844
Rogerio Chaves <rogeriocfj@gmail.com>
2013-10-30 08:28:32 -0200

    build fix

---
+++ web.js
@@ -0,0 +1,8 @@
+var gzippo = require('gzippo');
+var express = require('express');
+var app = express();
+ 
+app.use(express.logger('dev'));
+app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.listen(process.env.PORT || 5000);
+console.log("Server running on port " + (process.env.PORT || 5000))
